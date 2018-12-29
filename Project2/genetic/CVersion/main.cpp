1dd52bf679c8116708914021fd29d6e72f1ec750
hongwen000 <hongwen000@outlook.com>
2018-12-29 15:54:14 +0800

    c

--- Project2/genetic/CVersion/main.cpp
+++ Project2/genetic/CVersion/main.cpp
@@ -1,36 +1,45 @@
 #include <random>
+#include "set"
+#include <cmath>
 #include <iostream>
 #include "array"
 #include "algorithm"
 #include "vector"
 #include "cstdlib"
-#define USE_DEBUG
+#include "fstream"
 using namespace std;
-#define GENE_CNT 13
-using chrom_t = array<double, GENE_CNT>;
 
-std::vector<chrom_t> random_gene(int N)
-{
-    std::vector<chrom_t > ret;
-    srand(time(nullptr));
-    for(int i = 0; i < N; ++i)
-    {
-        for(int j = 0; j < GENE_CNT; ++j)
-        {
-            ret[i][j] = random() % 20 - 10;
-        }
-    }
-    return ret;
-}
+#include "reversi.h"
 
 std::vector<chrom_t> group;
 std::vector<chrom_t> next_group;
 std::vector<int> score;
 int iter = 0;
+time_t rawtime;
+struct tm * timeinfo;
+static char time_buffer[80];
+std::string timestr;
+
+std::random_device rd;
+std::mt19937 gen(rd());
+
+void random_gene(int N)
+{
+    std::uniform_int_distribution<> dis(-10, 10);
+
+    for(int i = 0; i < N; ++i)
+    {
+        group.push_back(chrom_t{});
+        for(int j = 0; j < GENE_CNT; ++j)
+        {
+            group[i][j] = dis(gen);
+        }
+    }
+}
+
 bool end_evolution(int N, int round)
 {
     double total_pk = round * N;
-#ifdef USE_DEBUG
     if(score.empty())
         cout << "Begin" << endl;
     else
@@ -43,15 +52,33 @@ bool end_evolution(int N, int round)
         cout << endl;
         cout << "  Best is " << *max_element(score.begin(), score.end()) / total_pk << endl;
     }
-#endif
+    if(!score.empty())
+    {
+        vector<size_t> idx(score.size());
+        iota(idx.begin(), idx.end(), 0);
+        sort(idx.begin(), idx.end(),
+             [](auto i1, auto i2) {return score[i1] > score[i2];});
+        system(("mkdir -p ret" + timestr).data());
+        ofstream f("ret" + timestr + "/" + std::to_string(iter) + ".txt", std::ios::app);
+        for(int j = 0; j < N; ++j)
+        {
+            f << score[idx[j]] << ": ";
+            for(auto i = 0; i < GENE_CNT; ++i)
+            {
+                f << group[idx[j]][i] << ",";
+            }
+            f << endl;
+        }
+        f.close();
+    }
     return iter > 10000;
 }
 
 /*
- * 适应度函数
- * 采用循环赛制
- * round: 比赛轮数
- * 返回值：每个染色体的适应度
+ * ?????
+ * ??????
+ * round: ????
+ * ?????????????
  */
 std::vector<int> calc_fit(int N, int round)
 {
@@ -62,27 +89,47 @@ std::vector<int> calc_fit(int N, int round)
     }
     for(int k = 0; k < round; ++k)
     {
+        int draw = 0;
         for(int i = 0; i < N; ++i)
         {
-            for(int j = 0; j < N; ++j)
+            int sum = 0;
+#pragma omp parallel for reduction(+:sum) num_threads(26)
+            for(int j = i + 1; j < N; ++j)
             {
                 auto ret = pk(group[i], group[j]);
-                score[i] += ret;
-                score[j] -= ret;
+                if(ret == 1)
+                {
+//                    printf("%d vs %d, %d wins\n", i, j, i);
+                    sum+=1;
+                }
+                else if(ret == -1)
+                {
+//                    printf("%d vs %d, %d wins\n", i, j, j);
+                    score[j]++;
+                }
             }
+            score[i] += sum;
         }
+//        auto ss = std::accumulate(score.begin(), score.end(),draw);
+//        if(ss == N * (N-1) / 2)
+//            throw(std::runtime_error{"?"});
+//        else
+//        {
+//            cout << ss << " " << N * (N-1) / 2 << endl;
+//            throw(std::runtime_error{"??"});
+//        }
     }
-    auto m = std::min_element(score.begin(), score.end());
-    if(*m < 0)
-        for(auto&i: score) i -= *m;
+//    auto m = std::min_element(score.begin(), score.end());
+//    if(*m < 0)
+//        for(auto&i: score) i -= *m;
     return score;
 }
 
 /*
- * 染色体交叉
- * p1: 双亲之一
- * p2: 双亲之一
- * 返回值： 交叉的到的新染色体
+ * ?????
+ * p1: ????
+ * p2: ????
+ * ???? ?????????
  */
 chrom_t cross_chrom(const chrom_t& p1, const chrom_t& p2)
 {
@@ -91,11 +138,9 @@ chrom_t cross_chrom(const chrom_t& p1, const chrom_t& p2)
     {
         v.push_back(i);
     }
-    std::random_device rd;
-    std::mt19937 g(rd());
-    std::shuffle(v.begin(), v.end(), g);
+    std::shuffle(v.begin(), v.end(), gen);
     std::uniform_int_distribution<> dis(0, GENE_CNT);
-    auto sp = dis(g);
+    auto sp = dis(gen);
     chrom_t new_gene;
     for(int i = 0; i < sp; ++i)
     {
@@ -108,16 +153,31 @@ chrom_t cross_chrom(const chrom_t& p1, const chrom_t& p2)
     return new_gene;
 }
 
-/*
- * 基因突变
- * gene: 进行突变的基因
- * R: 突变概率
- * D: 最大突变程度
- */
+void normalize(chrom_t & gene)
+{
+    int too_small = 1;
+    for(int i = 0; i < GENE_CNT; ++i)
+    {
+        if(abs(gene[i]) > 1) too_small--;
+    }
+    if(too_small >= 0)
+    {
+        for(int i = 0; i < GENE_CNT; ++i)
+            gene[i] *= 10;
+    }
+    bool too_large = false;
+    for(int i = 0; i < GENE_CNT; ++i)
+    {
+        if(abs(gene[i]) > 100000) too_large = true;
+    }
+    if(too_large)
+    {
+        for(int i = 0; i < GENE_CNT; ++i)
+            gene[i] /= 10.0;
+    }
+}
 void mutate_gene(chrom_t& gene, double R, double D)
 {
-    std::random_device rd;  //Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
     std::uniform_real_distribution<> dis(0, 1.0);
     std::uniform_real_distribution<> dis2(-1.0, 1.0);
     for(int i = 0; i < GENE_CNT; ++i)
@@ -128,30 +188,29 @@ void mutate_gene(chrom_t& gene, double R, double D)
         }
     }
 }
-/*
-遗传算法
-:param N: 种群数量
-:param M: 克隆数量
-:param R: 基因突变率
-:param round: 执行循环赛测试适应度的轮数
-:return: 生成的最优染色体
- */
+
 chrom_t genetic(int N, int M, double R, double D, int round)
 {
-    if(N < 2)
+    if(N < 2 || M > N)
     {
         throw(std::exception{});
     }
-    group = random_gene(N);
-    std::random_device rd;
-    std::mt19937 gen(rd());
+    random_gene(N);
     while (!end_evolution(N, round))
     {
         auto fitness = calc_fit(N, round);
         next_group.clear();
         std::discrete_distribution<> d(fitness.begin(), fitness.end());
-        for(int i = 0; i < M; ++i)
-            next_group.push_back(group[d(gen)]);
+        std::set<int> choices;
+        while (choices.size() < M)
+        {
+            auto choice = d(gen);
+            if(choices.count(choice))
+                continue;
+            choices.insert(choice);
+            normalize(group[choice]);
+            next_group.push_back(group[choice]);
+        }
         for(int i = 0; i < (N-M); ++i)
         {
             auto idx1 = d(gen);
@@ -164,7 +223,20 @@ chrom_t genetic(int N, int M, double R, double D, int round)
             auto& p2 = group[idx2];
             auto new_gene = cross_chrom(p1, p2);
             mutate_gene(new_gene, R, D);
+            normalize(new_gene);
+            next_group.push_back(new_gene);
         }
+        group = next_group;
     }
     return group[distance(max_element(score.begin(), score.end()), score.begin())];
+}
+
+int main()
+{
+    time (&rawtime);
+    timeinfo = localtime(&rawtime);
+    timestr = string(time_buffer);
+    strftime(time_buffer,sizeof(time_buffer),"%Y-%m-%d-%H-%M-%S",timeinfo);
+    timestr = string(time_buffer);
+    genetic(26, 3, 0.05, 1, 1);
 }
--- Project2/genetic/CVersion/reversi.h
+++ Project2/genetic/CVersion/reversi.h
@@ -2,24 +2,20 @@
 #define REVERSI_H
 #include <cstdint>
 #include <array>
+#include "cstdlib"
+#include "cstdint"
+#include "climits"
 using namespace std;
-#define GAMESCALE 8
 #include <utility>
 #include <functional>
-enum {EMPTY = 0, BLACK, WHITE};
-#define THINKINGLEVEL 10
+#define BLACK 1
+#define WHITE -1
+#define THINKINGLEVEL 5
 
-using State = std::array<char, GAMESCALE * GAMESCALE>;
-
-extern const int checkDirection[8][2];
-bool inBoundary(int x, int y);
-pair<int,int> getXY(int id);
-int getID(int x, int y);
-State getNextState(const State& s, int id, int color);
-std::array<int, GAMESCALE * GAMESCALE> getAvail(const State& s, int color);
-int positionalValue(const State& s, int color);
-using valueFunc = std::function<int(const State& s, int color)>;
-int MinMax(const State& s, int color, int limit, const valueFunc& V, int & bestMove);
-int AlphaBeta(const State& s, int color, int limit, const valueFunc& V, int alpha, int beta, int & bestMove);
-
-#endif // REVERSI_H
+//#define USE_GENE_DEBUG
+//#define USE_DEBUG
+#define GENE_CNT 13
+using chrom_t = array<double, GENE_CNT>;
+using chrom_t = array<double, GENE_CNT>;
+int pk(const chrom_t& g1, const chrom_t & g2);
+#endif //
